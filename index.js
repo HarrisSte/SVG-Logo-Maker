aaa85844236940f1aeade6bfbb9a5a42bf964f1c
Stephanie <126029841+HarrisSte@users.noreply.github.com>
2023-05-23 18:57:33 -0400

    Added correct version of node & added createSVG.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const inquirer = require("inquirer");
 const { writeFile } = require("fs");
 const { createSVG } = require("./lib/createSVG");
-const shapes = require("/lib/shapes");
+const shapes = require("./lib/shapes");
 
 // Questions for users to answer to create logo
 const prompts = [
@@ -15,7 +15,7 @@ const prompts = [
   {
     type: "input",
     name: "textColor",
-    message: "Enter the text color:",
+    message: "Enter the text color (keyword or hexadecimal number):",
   },
   {
     type: "list",
@@ -26,6 +26,27 @@ const prompts = [
   {
     type: "input",
     name: "shapeColor",
-    message: "Enter the shape color:",
+    message: "Enter the shape color (keyword or hexadecimal number):",
   },
 ];
+
+// Prompt the user with the questions
+inquirer
+  .prompt(prompts)
+  .then((answers) => {
+    if (answers.text.length > 3) {
+      console.log("Must enter a value of no more than 3 characters");
+    } else {
+      const logoData = createSVG(answers);
+      writeFile("logo.svg", logoData, (err) => {
+        if (err) {
+          console.error("Error writing logo file:", err);
+        } else {
+          console.log("Logo created successfully!");
+        }
+      });
+    }
+  })
+  .catch((error) => {
+    console.error("Error occurred:", error);
+  });
